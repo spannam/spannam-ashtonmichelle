926db4052919eeedb4048440f43e2d35c523df32
spannam <spannam@miamioh.edu>
2017-05-18 12:24:33 -0400

    created js file

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// jQuery script to nav scroll to section
+
+$(document).ready(function(){
+  $('.nav li a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
+
+//jQuery script to page-scroll & animate fa-icon
+
+$(document).ready(function() { 
+  $('a.page-scroll').click(function() {  
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')   && location.hostname == this.hostname) {   
+      var $target = $(this.hash);   
+      $target = $target.length && $target    || $('[name=' + this.hash.slice(1) + ']');   
+      if ($target.length) {    
+        var targetOffset = $target.offset().top;    
+        $('html,body')    .animate({
+          scrollTop: targetOffset
+        }, 1000);    
+        return false;   
+      }  
+    } 
+  });
+});
